ac8fa91de5701998ce144130bf1d45c63d3b57be
isuru <isurumanod99@gmail.com>
2023-07-25 19:25:38 +0530

    add comment

--- async_task/utils.js
+++ async_task/utils.js
@@ -3,6 +3,8 @@ const pause = (seconds = 1000) => {
 
     const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
+
+            // i use ready for testing
             isReady = true
 
             isReady ? resolve("ready") : reject("not ready")
